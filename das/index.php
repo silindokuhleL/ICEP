704b3c39be45483b4a9f6b0e4a730858d2636500
SilindokuhleL <silindokuhlelmapiyeye@gmail.com>
2020-01-11 15:51:49 +0200

    my second push to the repo

---
+++ das/index.php
@@ -0,0 +1,53 @@
+<?php include('header.php'); ?>
+
+	<!-- this is for welcome -->
+	<div class="content">
+		<article>
+			<img src="img/welcome1.png" alt="welcome msg">
+			<p class="text-justify">
+			</p>
+		</article>
+
+
+	
+	</div><br>
+
+	<!-- nivo slider starts -->
+	<div class="col-md-12 sliderImg">
+		<img src="img/doctor5.jpg" alt="">
+		
+		<img src="img/hstu8.jpg"   alt="">
+		
+	</div>
+	<!-- nivo slider ends -->
+
+	<!-- main Content -->
+	<div class="main_content">
+		<div class="col-md-8">
+			<article>
+			<h3 style="font-weight: bold;font-family:inherit;">Finds Doctors from anywhere anytime!</h3><hr>
+				<p class="text-justify">
+				
+				</p>
+			</article>
+		</div>
+		
+
+          
+    </div>
+
+ 	<?php include('footer.php'); ?>
+
+
+	
+</div>	<!--  containerFluid Ends -->
+
+
+
+	<script src="js/jquery-1.9.0.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+	 
+	
+</body>
+</html>
